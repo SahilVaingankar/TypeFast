3be615b5e4a21c5e35b43da69ba074db33c972f9
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-05-01 21:17:30 +0530

    created navbarOptions file

---
+++ src/components/navbar/navbarOptions.ts
@@ -0,0 +1,25 @@
+interface Options {
+  Words: string[];
+  Lines: string[];
+  Code: null;
+}
+
+const navbarOptions: Options = {
+  Words: ["A - Z", "10 words", "25 words", "50 words", "75 words", "100 words"],
+  Lines: [
+    "5 lines",
+    "10 lines",
+    "20 lines",
+    "30 lines",
+    "40 lines",
+    "50 lines",
+    "60 lines",
+    "70 lines",
+    "80 lines",
+    "90 lines",
+    "100 lines",
+  ],
+  Code: null,
+};
+
+export default navbarOptions;
