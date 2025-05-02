f4016cd0d102ab9fcb4819d4e77056fa8cd34209
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-05-02 17:52:47 +0530

    fixing more navbar problems

--- src/components/navbar/NavbarBtns.tsx
+++ src/components/navbar/NavbarBtns.tsx
@@ -5,8 +5,16 @@ const NavbarBtns = ({ navbarOptions }: any) => {
   const [selectedBtn, setSelectedBtn] = useState<string>("");
 
   const handleClick = (btn: string) => {
-    setIsOpen(!isOpen);
-    setSelectedBtn(btn);
+    // if (btn == "Code") {
+    //   console.log(btn);
+    // }
+    if (isOpen) {
+      setIsOpen(false);
+      setSelectedBtn("");
+    } else {
+      setIsOpen(!isOpen);
+      setSelectedBtn(btn);
+    }
   };
 
   const handleHover = (btn: string) => {
@@ -15,7 +23,14 @@ const NavbarBtns = ({ navbarOptions }: any) => {
 
   return (
     <>
-      <button className="border-2 border-white rounded-xl py-0.5 w-full min-w-23 text-white font-semibold hover:bg-white hover:text-blue-700 hover:border-blue-300 hover:cursor-pointer transition-all delay-75">
+      <button
+        className="border-2 border-white rounded-xl py-0.5 w-full min-w-23 text-white font-semibold hover:bg-white hover:text-blue-700 hover:border-blue-300 hover:cursor-pointer transition-all delay-75"
+        onClick={() => handleClick("A-Z")}
+        onMouseEnter={() => handleHover("A-Z")}
+        onBlur={() => {
+          setIsOpen(false);
+          setSelectedBtn("");
+        }}>
         A-Z
       </button>
 
@@ -32,14 +47,18 @@ const NavbarBtns = ({ navbarOptions }: any) => {
             }}>
             {btn}
           </button>
-          {selectedBtn === btn && navbarOptions[btn] ? (
-            <ul className="absolute text-center top-10 left-0 border-2 rounded-lg py-0.5 w-full min-w-23">
+          {isOpen && selectedBtn === btn && navbarOptions[btn] ? (
+            <ul className="absolute text-center top-10 left-0 border-2 rounded-lg py-0.5 w-full min-w-23 bg-gray-200 overflow-hidden">
               {navbarOptions[btn].slice(0, -1).map((option: string) => (
-                <li className="border-b-2" key={option}>
+                <li
+                  className="border-b-2 bg-white hover:bg-gray-200"
+                  key={option}>
                   {option}
                 </li>
               ))}
-              <li>{navbarOptions[btn][navbarOptions[btn].length - 1]}</li>
+              <li className="bg-white hover:bg-gray-200 mb-[-2]">
+                {navbarOptions[btn][navbarOptions[btn].length - 1]}
+              </li>
             </ul>
           ) : null}
           {/* <li>
