0aa69cb70027c98c1837641d899b065c1f342c39
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-05-02 10:41:58 +0530

    created handleClick function for NavbarBtns dropdowm menu

--- src/components/navbar/NavbarBtns.tsx
+++ src/components/navbar/NavbarBtns.tsx
@@ -1,4 +1,12 @@
+import { useState } from "react";
+
 const NavbarBtns = ({ navbarOptions }: any) => {
+  const [isOpen, setisOpen] = useState("");
+
+  const handleClick = (btn: string) => {
+    setisOpen(btn);
+  };
+
   return (
     <>
       <button className="border-2 border-white rounded-xl py-0.5 w-full min-w-23 text-white font-semibold hover:bg-white hover:text-blue-700 hover:border-blue-300 hover:cursor-pointer transition-all delay-75">
@@ -9,10 +17,11 @@ const NavbarBtns = ({ navbarOptions }: any) => {
         <div className="relative w-full">
           <button
             className="border-2 border-white rounded-xl py-0.5 w-full min-w-23 text-white font-semibold hover:bg-white hover:text-blue-700 hover:border-blue-300 hover:cursor-pointer transition-all delay-75"
-            key={btn}>
+            key={btn}
+            onClick={() => handleClick(btn)}>
             {btn}
           </button>
-          {navbarOptions[btn] ? (
+          {isOpen === btn && navbarOptions[btn] ? (
             <ul className="absolute text-center top-10 left-0 border-2 rounded-lg py-0.5 w-full min-w-23">
               {navbarOptions[btn].slice(0, -1).map((option: string) => (
                 <li className="border-b-2" key={option}>
