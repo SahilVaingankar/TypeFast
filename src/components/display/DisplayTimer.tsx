7e35430623bfc4aaee94b501025a5ece7c393bda
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-05-04 17:29:08 +0530

    added restart/stop buttons to timer in DisplatTimer comoponent

--- src/components/display/DisplayTimer.tsx
+++ src/components/display/DisplayTimer.tsx
@@ -2,19 +2,19 @@ import { useEffect, useRef, useState } from "react";
 
 const Timer = () => {
   const [timer, setTimer] = useState("00 : 00 : 00");
-  const [clicked, setClicked] = useState(false);
+  const [running, setRunning] = useState(false);
   const startTime = useRef<number>(Date.now());
+  const currentTime = useRef<number>(0);
   const updateRef = useRef<number | undefined>(undefined);
-  const updatefunc = useRef<() => void>(() => {});
 
   useEffect(() => {
     const update = () => {
-      const currentTime = Date.now();
+      currentTime.current = Date.now();
 
       console.log("update");
       console.log(timer);
 
-      const elaspedTime = currentTime - startTime.current;
+      const elaspedTime = currentTime.current - startTime.current;
 
       const minutes = Math.floor((elaspedTime / (1000 * 60)) % 60)
         .toString()
@@ -30,7 +30,6 @@ const Timer = () => {
     };
 
     updateRef.current = setInterval(update, 100);
-    updatefunc.current = update;
 
     return () => {
       clearInterval(updateRef.current);
@@ -38,12 +37,14 @@ const Timer = () => {
   }, []);
 
   const stop = () => {
-    if (!clicked) {
-      clearInterval(updateRef.current);
-      setClicked(false);
-    } else {
-      updatefunc.current();
-    }
+    clearInterval(updateRef.current);
+    setTimer("00 : 00 : 00");
+  };
+
+  const restart = () => {
+    currentTime.current = Date.now();
+    startTime.current = Date.now();
+    setTimer("00 : 00 : 00");
   };
 
   return (
@@ -51,8 +52,11 @@ const Timer = () => {
       <div className="bg-black py-2 text-white flex justify-center items-center">
         {timer}
       </div>
-      <button className="bg-blue-700 py-2 text-white" onClick={stop}>
+      <button className="bg-blue-700 p-2 m-2 text-white" onClick={stop}>
         Stop
+      </button>
+      <button className="bg-blue-700 p-2 m-2 text-white" onClick={restart}>
+        restart
       </button>
     </div>
   );
