2856d59569b5e6b1817ab318879b48dc15107cd5
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-05-05 13:57:26 +0530

    added buttons on display footer section

--- src/components/display/footer/DisplayBtns.tsx
+++ src/components/display/footer/DisplayBtns.tsx
@@ -1,23 +1,25 @@
 // import Buttons from "./Buttons";
 
+import { FaBackward, FaForward, FaPlay, FaRedo, FaStop } from "react-icons/fa";
+
 const DisplayBtns = () => {
   return (
     <footer className="bg-blue-700 w-full">
       <div className="py-2 flex justify-center items-center gap-0.5 h-full">
         <button className="h-11 w-13 bg-gray-400 border-2 border-white mr-10 lg:mr-15">
-          X
+          <FaBackward className="m-auto" />
         </button>
         <button className="h-10 w-10 bg-gray-400 border-2 border-white mr-20">
-          X
+          <FaRedo className="m-auto" />
         </button>
-        <button className="absolute h-14 w-14 bg-gray-400 border-2 border-white rounded-full">
-          X
+        <button className="absolute h-14 w-14 border-2 border-white rounded-full">
+          <FaPlay className="m-auto text-white h-7 w-7 pl-1" />
         </button>
         <button className="h-10 w-10 bg-gray-400 border-2 border-white">
-          X
+          <FaStop className="text-red-500 m-auto" />
         </button>
         <button className="h-11 w-13 bg-gray-400 border-2 border-white ml-10 lg:ml-15">
-          X
+          <FaForward className="m-auto" />
         </button>
       </div>
     </footer>
