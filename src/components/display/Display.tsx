6f601a8b80f04c8a708ec11640c1aaac041b542d
Sahil Vaingankar <sahilvaingankar21@gmail.com>
2025-05-08 20:40:02 +0530

    fixed navbar blur effect by removing isOpen state ard relying sololy on selectedBtn state

--- src/components/display/Display.tsx
+++ src/components/display/Display.tsx
@@ -59,7 +59,9 @@ const Display = () => {
   }, [running]);
 
   return (
-    <section className="flex flex-col items-center min-w-1 grow-1 h-[calc(100vh-50px)] border-2 leading-[100%]">
+    <section
+      className="flex flex-col items-center min-w-1 grow-1 h-[calc(100vh-50px)] border-2 leading-[100%]"
+      onClick={() => setSelectedBtn("")}>
       <DisplayTimer {...{ timer }} />
       <DisplayScreen {...{ running, processing }} />
       <DisplayBtns
--- src/components/navbar/NavbarBtns.tsx
+++ src/components/navbar/NavbarBtns.tsx
@@ -26,17 +26,19 @@ const NavbarBtns = ({ navbarOptions }: any) => {
       setSelectedBtn(btn);
       // setIsOpen(!isOpen);
       !navbarOptions[btn] && console.log(btn);
-    } else {
-      setSelectedBtn("");
     }
+    //  else {
+    //   setSelectedBtn("");
+    // }
   };
 
   const handleHover = (btn: string) => {
     if (!running && !processing) {
-      isOpen && setSelectedBtn(btn);
-    } else {
-      setSelectedBtn("");
+      setSelectedBtn(btn);
     }
+    //  else {
+    //   setSelectedBtn("");
+    // }
   };
 
   return (
@@ -55,18 +57,21 @@ const NavbarBtns = ({ navbarOptions }: any) => {
         <div className="relative w-full" key={btn}>
           <button
             className={`border-2 rounded-xl py-0.5 w-full min-w-23 font-semibold ${
-              isOpen && selectedBtn === btn && navbarOptions[btn]
+              selectedBtn === btn && navbarOptions[btn]
                 ? "bg-white text-blue-700 border-blue-300"
                 : "text-white border-white"
             } hover:bg-white hover:text-blue-700 hover:border-blue-300 hover:cursor-pointer transition-all delay-75`}
             onClick={() => handleClick(btn)}
             onMouseEnter={() => {
               handleHover(btn);
-              setIsOpen(true);
+            }}
+            onBlur={() => {
+              setSelectedBtn("");
+              setIsOpen(false);
             }}>
             {btn}
           </button>
-          {!running && isOpen && selectedBtn === btn && navbarOptions[btn] ? (
+          {!running && selectedBtn === btn && navbarOptions[btn] ? (
             <ul className="absolute text-center top-10 left-0 border-2 rounded-lg py-0.5 w-full min-w-23 bg-gray-200 overflow-hidden">
               {navbarOptions[btn].slice(0, -1).map((option: string) => (
                 <li
